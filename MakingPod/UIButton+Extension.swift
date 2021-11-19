c9720967449f49017d5969b972a25d85e18b4f12
Joao-LDS <joao_l_d_s@hotmail.com>
2021-11-19 19:01:22 -0300

    Initial Commit

---
+++ MakingPod/UIButton+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIButton+Extension.swift
+//  MakingPod
+//
+//  Created by João Luis dos Santos on 19/11/21.
+//
+
+import UIKit
+
+extension UIButton {
+    public func setBorder(with color: UIColor, width: CGFloat) {
+        layer.borderColor = color.cgColor
+        layer.borderWidth = width
+    }
+}
